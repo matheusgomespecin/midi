18e1a06e539d354256379eeab523bc9205acb339
matheusgomespecin <134521816+matheusgomespecin@users.noreply.github.com>
2023-10-25 11:40:05 -0300

    todos os sons

--- main.js
+++ main.js
@@ -8,4 +8,46 @@ function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
 }
 
-document.querySelector('.tecla.clap').onclick = tocaSomClap
+document.querySelector('.tecla.clap').onclick = tocaSomClap;
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_tim').play();
+}
+
+document.querySelector('.tecla.tim').onclick = tocaSomTim;
+
+function tocaSomPuff(){
+    document.querySelector('#som_tecla_puff').play();
+}
+
+document.querySelector('.tecla.puff').onclick = tocaSomPuff;
+
+function tocaSomSplash(){
+    document.querySelector('#som_tecla_splash').play();
+}
+
+document.querySelector('.tecla.splash').onclick = tocaSomSplash;
+
+function tocaSomToim(){
+    document.querySelector('#som_tecla_toim').play();
+}
+
+document.querySelector('.tecla.toim').onclick = tocaSomToim;
+
+function tocaSomPsh(){
+    document.querySelector('#som_tecla_psh').play();
+}
+
+document.querySelector('.tecla.psh').onclick = tocaSomPsh;
+
+function tocaSomTic(){
+    document.querySelector('#som_tecla_tic').play();
+}
+
+document.querySelector('.tecla.tic').onclick = tocaSomTic;
+
+function tocaSomTom(){
+    document.querySelector('#som_tecla_tom').play();
+}
+
+document.querySelector('.tecla.tom').onclick = tocaSomTom;
