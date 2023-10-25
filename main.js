42deb10a8c4723a0e2613e9e0d8a5c0e30c9d456
matheusgomespecin <134521816+matheusgomespecin@users.noreply.github.com>
2023-10-25 11:22:23 -0300

    som 2 e 3

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ function tocaSomPom(){
 }
 
 document.querySelector('.tecla.pom').onclick = tocaSomPom;
+
+function tocaSomClap(){
+    document.querySelector('#som_tecla_clap').play();
+}
+
+document.querySelector('.tecla.clap').onclick = tocaSomClap;
+
+function tocaSomTim(){
+    document.querySelector('#som_tecla_Tim').play();
+}
+
+document.querySelector('.tecla.Tim').onclick = tocaSomTim;
